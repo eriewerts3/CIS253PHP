a6a82a3476512caf8a306bf90cedc2d3523210a9
eriewerts3 <55771100+eriewerts3@users.noreply.github.com>
2022-01-25 16:45:56 -0600

    Ch 2 example exercises and reinforcement exercises

---
+++ CIS253/ch2/OddNumbers.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Odd Numbers</title>
+        <meta http-equiv= "content-type" content= "text/html; charset=iso-8859-1"/>
+    </head>
+    <body>
+        <?php
+            echo "<p> The odd numbers between 1 and 100 are",
+            $i=0;
+            while ($i <= 100) {
+                if ($i % 2 != 0) {
+                    echo "$i,"
+                }
+                $i++
+            }
+            echo "</p>"
+        ?>
+    </body>
+</html>
