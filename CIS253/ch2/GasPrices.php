fd67cdb6730e2316d914188289b94168a59b382f
eriewerts3 <55771100+eriewerts3@users.noreply.github.com>
2022-01-25 17:19:02 -0600

    Final touches for Ch2 exercices

--- CIS253/ch2/GasPrices.php
+++ CIS253/ch2/GasPrices.php
@@ -7,7 +7,7 @@
     <body>
         <?php
             $GasPrice = 2.57;
-            if ($GasPrice <= 2 && $GasPrice >= 3) {
+            if ($GasPrice >= 2 && $GasPrice <= 3) {
                 echo "<p> Gas prices are between $2.00 and $3.00.</p>";
             } else {
                 echo "<p>Gas Prices are not between $2.00 and $3.00</p>";
--- CIS253/ch2/OddNumbers.php
+++ CIS253/ch2/OddNumbers.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <?php
-            echo "<p> The odd numbers between 1 and 100 are";
+            echo "<p> The odd numbers between 1 and 100 are ";
             $i=0;
             while ($i <= 100) {
                 if ($i % 2 != 0) {
