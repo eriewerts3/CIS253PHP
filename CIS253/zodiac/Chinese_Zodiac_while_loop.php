900881a8836a7241226c4a100a3495b015fbb910
eriewerts3 <55771100+eriewerts3@users.noreply.github.com>
2022-02-10 22:48:52 -0600

    Comp checks 1&2, zodiac and ch3 practice excercises

--- CIS253/zodiac/Chinese_Zodiac_while_loop.php
+++ CIS253/zodiac/Chinese_Zodiac_while_loop.php
@@ -1,32 +1,29 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <title>Chinese_Zodiac_for_loop</title>
+        <title>Chinese_Zodiac_While_loop</title>
         <meta http-equiv= "content-type" content= "text/html; charset=iso-8859-1"/>
     </head>
     <body>
+    <h1>Chinese Zodiac While loop</h1>
         <?php
             $SignNames = array("Rat","Ox","Tiger","Rabbit","Dragon","Snake","Horse","Goat","Monkey","Rooster","Dog","Pig");
 
-            echo "<table>/n"
-            echo "<tr>/n";
-            for ($i=0; $i<12; ++i){
-                echo "<th>$SignNames[$i]<br/>/n";
-                echo "<img src= 'images/animals/" . $SignNames[$i] . ".gif' alt='". $SignNames[$i] . "'/></th>/n";
+            echo "<table>\n";
+            echo "<tr>\n";
+            for ($i=0; $i<12; ++$i){
+                echo "<th>$SignNames[$i]<br/>\n";
+                echo "<img src= 'images/animals/" . $SignNames[$i] . ".gif' alt='". $SignNames[$i] . "'/></th>\n";
             }
-
-            $firstY= 1912; //Made this a variable for easy updates
+            
             $currentY= date("Y");// thought I would try this out
 
-            // for ($i=$firstY; $i<=$currentY; ++i) {
-            //     if ((($i-$firstY)%12)==0) {
-            //         echo "</tr><tr>";
-
+            $i=1912;
             while ($i <= $currentY) {
-                if ((($i-$firstY)%12)==0) {
+                if ((($i-1912)%12)==0) {
                 echo "</tr><tr>";
                 }
-                echo <td>.$i.</td>;
+                echo "<td>".$i."</td>";
                 ++$i;
             }
             echo "</tr></table>";
