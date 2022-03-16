126a4c7fabe026ab8f5c984fb793b251ba0eb8ba
eriewerts3 <55771100+eriewerts3@users.noreply.github.com>
2022-03-16 13:26:59 -0500

    BirthYear_ifelse debugging

--- CIS253/zodiac/includes/inc_site_layout.php
+++ CIS253/zodiac/includes/inc_site_layout.php
@@ -1,1 +1,49 @@
-[Insert site layout content here]
+<p>A Web template is a tool used to separate content from presentation in Web design, and for mass-production of Web documents. It is a basic component of a Web template system.</p>
+
+<p>Web templates can be used to set up any type of Web site. In its simplest sense, a Web template operates similarly to a form letter for use in setting up a Web site.</p>
+
+<p>Content (from a database), and “presentation specifications” (in a Web template), are combined (through the template engine) to mass-produce Web documents.</p>
+
+<h3>Template Uses</h3>
+
+<p>Web templates can be used by any individual or organization to set up their Web site. Once a template is purchased or downloaded, the user will replace all generic information included in the Web template with their own personal, organizational or product information. Templates can be used to:</p>
+
+<ul>
+    <li>Display personal information or daily activities as in a blog.</li>
+    <li>Sell products on-line.</li>
+    <li>Display information about a company or organization.</li>
+    <li>Display family history.</li>
+    <li>Display a gallery of photos.</li>
+    <li>Place music files such as mp3 on line for play through a Web browser.</li>
+    <li>Place videos on-line for public viewing.</li>
+    <li>To setup a private login area on-line.</li>
+</ul>
+
+<h3>Effective separation</h3>
+
+<p>A common goal among experienced Web developers is to develop and deploy applications that are flexible and easily maintainable. An important consideration in reaching this goal is the separation of business logic from presentation logic. Developers use Web template systems (with varying degrees of success) to maintain this separation.</p>
+
+<p>One difficulty in evaluating this separation is the lack of well-defined formalisms to measure when and how well it is actually met. There are, however, fairly standard heuristics that have been borrowed from the domain of software engineering. These include &aposinheritance&apos (based on principles of object-oriented programming); and the &aposTemplating and Generative programming&apos, (consistent with the principles of MVC separation). The precise difference between the various guidelines is subject to some debate, and some aspects of the different guidelines share a degree of similarity.</p>
+
+<h3>Flexible presentation</h3>
+
+<p>One major rationale behind “effective separation” is the need for maximum flexibility in the code and resources dedicated to the presentation logic. Client demands, changing customer preferences and desire to present a “fresh face” for pre-existing content often result in the need to dramatically modify the public appearance of Web content while disrupting the underlying infrastructure as little as possible.</p>
+
+<p>The distinction between “presentation” (front end) and “business logic” (infrastructure) is usually an important one, because:</p>
+<ul>
+    <li>the presentation source code language may differ from other code assets;</li>
+    <li>the production process for the application may require the work to be done at separate times and locations;</li>
+    <li>different workers have different skill sets, and presentation skills do not always coincide with skills for coding business logic;</li>
+    <li>code assets are easier to maintain and more readable when disparate components are kept separate and loosely coupled;</li>
+</ul>
+<h3>Reusability</h3>
+
+<p>Not all potential users of Web templates have the willingness and ability to hire developers to design a system for their needs. Additionally, some may wish to use the Web but have limited or no technical proficiency. For these reasons, a number of developers and vendors have released Web templates specifically for reuse by non-technical people. Although Web template reusability is also important for even highly-skilled and technically experienced developers, it is especially critical to those who rely on simplicity and “ready-made” Web solutions.</p>
+
+<p>Such “ready-made” Web templates are sometimes free, and easily made by an individual domestically. However, specialized Web templates are sometimes sold online. Although there are numerous commercial sites that offer Web templates for a licensing fee, there are also free and “open-source” sources as well.</p>
+
+<h3>Open source templates</h3>
+
+<p>The rise of the open source design movement has seen a slow but steady rise in the community of open source designers. Some sites offer open source templates in addition to other content.</p>
+
+<p>- from Wikipedia ( <a href="http://en.wikipedia.org/wiki/Web_template">http://en.wikipedia.org/wiki/Web_template</a>)</p>
